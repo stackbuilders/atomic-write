2f45142e0af11de42e2439b6e89414acf00d8240
William R. Arellano <warellano@stackbuilders.com>
2019-11-20 16:34:51 -0500

    Add binary mode versions of functions for ByteString

--- src/System/AtomicWrite/Writer/ByteString.hs
+++ src/System/AtomicWrite/Writer/ByteString.hs
@@ -10,13 +10,16 @@
 -- Provides functionality to dump the contents of a ByteString
 -- to a file.
 
-module System.AtomicWrite.Writer.ByteString (atomicWriteFile, atomicWriteFileWithMode) where
+module System.AtomicWrite.Writer.ByteString (atomicWriteFile, atomicWriteFileWithMode, atomicWriteFile', atomicWriteFileWithMode') where
 
-import System.AtomicWrite.Internal (closeAndRename, tempFileFor, maybeSetFileMode)
+import           System.AtomicWrite.Internal (closeAndRename, maybeSetFileMode,
+                                              tempFileFor)
 
-import System.Posix.Types (FileMode)
+import           System.Posix.Types          (FileMode)
 
-import Data.ByteString (ByteString, hPutStr)
+import           Data.ByteString             (ByteString, hPutStr)
+
+import           System.IO                   (hSetBinaryMode)
 
 -- | Creates or modifies a file atomically on POSIX-compliant
 -- systems while preserving permissions.
@@ -44,5 +47,37 @@ atomicWriteFileMaybeMode ::
   -> IO ()
 atomicWriteFileMaybeMode mmode path text =
   tempFileFor path >>= \(tmpPath, h) -> hPutStr h text
+                    >> closeAndRename h tmpPath path
+                    >> maybeSetFileMode path mmode
+
+-- | Creates or modifies a file atomically on POSIX-compliant
+-- systems while preserving permissions. The file is opened in
+-- binary mode.
+atomicWriteFile' ::
+  FilePath      -- ^ The path where the file will be updated or created
+  -> ByteString -- ^ The content to write to the file
+  -> IO ()
+atomicWriteFile' = atomicWriteFileMaybeMode' Nothing
+
+-- | Creates or modifies a file atomically on POSIX-compliant
+-- systems and updates permissions. The file is opened in binary
+-- mode.
+atomicWriteFileWithMode' ::
+  FileMode
+  -> FilePath      -- ^ The path where the file will be updated or created
+  -> ByteString -- ^ The content to write to the file
+  -> IO ()
+atomicWriteFileWithMode' mode =
+  atomicWriteFileMaybeMode' $ Just mode
+
+-- | Helper function for opening the file in binary mode.
+atomicWriteFileMaybeMode' ::
+  Maybe FileMode
+  -> FilePath      -- ^ The path where the file will be updated or created
+  -> ByteString -- ^ The content to write to the file
+  -> IO ()
+atomicWriteFileMaybeMode' mmode path text =
+  tempFileFor path >>= \(tmpPath, h) -> hSetBinaryMode h True
+                    >> hPutStr h text
                     >> closeAndRename h tmpPath path
                     >> maybeSetFileMode path mmode
